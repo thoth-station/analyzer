7cda4440d6f347dc86824d27f3a72e3b053e1b11
Fridolin Pokorny <fridolin@redhat.com>
2018-03-26 12:57:19 +0200

    Version 0.0.5

--- thoth/analyzer/__init__.py
+++ thoth/analyzer/__init__.py
@@ -1,7 +1,7 @@
 """Shared code logic in Thoth analyzers."""
 
 __title__ = 'thoth-analyzer'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from .cli import print_command_result
 from .command import CommandError
