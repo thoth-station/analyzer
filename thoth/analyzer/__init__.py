ba70ecff16ccccc193cc5c98adab84ddc9c4cb5c
Christoph Görn <goern@redhat.com>
2018-11-08 13:37:42 +0100

    lets have a simple version string
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/analyzer/__init__.py
+++ thoth/analyzer/__init__.py
@@ -5,7 +5,6 @@ from .command import CommandError
 from .command import CommandResult
 from .command import run_command
 
-from thoth.common import __version__ as __common__version__
 
 __title__ = 'thoth-analyzer'
-__version__ = f"0.1.0+common.{__common__version__}"
+__version__ = '0.1.0'
